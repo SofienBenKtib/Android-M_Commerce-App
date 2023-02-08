5e284a5a40afd30e0f1874e1d489ff568611a742
sofien1-admin <SofienBenKtib>
2023-02-09 00:12:26 +0100

    initial commit

---
+++ app/src/main/java/com/example/socialmediaapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.socialmediaapp
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.core.view.isVisible
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.login)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val userName: String = "Sofien"
+        val userPassword: String = "123abc"
+        loginButton.setOnClickListener {
+            //USERNAME AUTHENTIFICATION VERIFICATION
+            if (email.text.toString().equals(userName) && password.text.toString().equals(userPassword) ) {
+                Toast.makeText(this, "Welcome back $userName", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Please enter a valid username", Toast.LENGTH_SHORT).show()
+            }
+            if (password.text.isEmpty()) {
+                Toast.makeText(this, "Please enter a valid password", Toast.LENGTH_SHORT).show()
+            } else {
+                val handler = Handler()
+                val runnable = Runnable {
+                    progressBar.isVisible = true
+                    loginButton.setBackgroundColor(Color.GREEN)
+                }
+                handler.postDelayed(runnable, 1000)
+                progressBar.isVisible = false
+                loginButton.setText("Please wait...")
+            }
+        }
+    }
+}
