cfca001c6af82e06ae48c1083b2d7005ce40b366
sofien1-admin <SofienBenKtib>
2023-02-13 16:08:32 +0100

    updates in home screen

--- app/src/main/java/com/example/socialmediaapp/Home.kt
+++ app/src/main/java/com/example/socialmediaapp/Home.kt
@@ -2,10 +2,16 @@ package com.example.socialmediaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val favorite=findViewById<ImageButton>(R.id.favorite)
+        favorite.setOnClickListener{
+            Toast.makeText(this,"Test works",Toast.LENGTH_LONG).show()
+        }
     }
 }
